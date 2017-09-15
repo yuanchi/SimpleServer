4e2569a64dd77b7fa3d52e046beae85261aedb65
yuanchi <eeit54@gmail.com>
2017-09-15 17:01:32 +0800

    MiddlewareBuilder.java add class comments.

--- server-core/src/main/java/com/jerrylin/server/handlers/MiddlewareBuilder.java
+++ server-core/src/main/java/com/jerrylin/server/handlers/MiddlewareBuilder.java
@@ -3,7 +3,22 @@ package com.jerrylin.server.handlers;
 import io.undertow.server.HttpHandler;
 
 import java.util.function.Function;
-
+/**
+ * for the experiment and learning,<br>
+ * copying from
+ * <a href="https://github.com/StubbornJava/StubbornJava/blob/master/stubbornjava-undertow/src/main/java/com/stubbornjava/undertow/handlers/MiddlewareBuilder.java">this.</a>
+ * <br><br>
+ * One of the most powerful features of Undertow is the handler chain,<br>
+ * but its composing order is contrary to the executing order,<br>
+ * or need to nest embed multiple handlers.
+ * <br><br>
+ * In any case, it seems more verbose or counter intuitive.
+ * <br><br>
+ * Via this builder, it can keep the format succinct,<br>
+ * and make the execution order as expressed.
+ * <br><br>
+ * I write some <a href="https://github.com/yuanchi/SimpleServer/blob/master/server-core/src/test/java/com/jerrylin/server/handlers/MiddlewareBuilderTest.java">test cases</a> to present.
+ */
 public class MiddlewareBuilder {
 	private final Function<HttpHandler, HttpHandler> function;
 	
